1fc572bdf2ffbfbe6bd5cc84a154d2aa8a5247a6
rakati <ouhaddounouredine@gmail.com>
2024-02-09 07:14:05 -0500

    add function to get  data from dealer service api

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -24,6 +24,46 @@ def get_request(url, **kwargs):
 # Create a `post_request` to make HTTP POST requests
 # e.g., response = requests.post(url, params=kwargs, json=payload)
 
+def get_dealers_from_cf(url, **kwargs):
+    '''
+    the function request data from dealers api service using `get_request`
+    function, parse return json data into a CarDealer object list
+
+    Parameters:
+    ----------
+        url: str
+            url of the api service to fetch data from using get method.
+        **kargs:
+            parameters to use in the query string withing request url
+    
+    Returns:
+    --------
+        parsed json result stored in a list of CarDealer objects
+    '''
+    results = []
+    # Call get_request with a URL parameter
+    json_result = get_request(url, kwargs)
+    if json_result:
+        # Get the row list in JSON as dealers
+        dealers = json_result
+        
+        # For each dealer object
+        for dealer in dealers:
+            # Get its content in `doc` object
+            dealer_doc = dealer
+            print("DEaler",dealer_doc)
+            # Create a CarDealer object with values in `doc` object
+            dealer_obj = CarDealer(address=dealer_doc["address"],
+                                   city=dealer_doc["city"],
+                                   full_name=dealer_doc["full_name"],
+                                   id=dealer_doc["id"],
+                                   lat=dealer_doc["lat"],
+                                   long=dealer_doc["long"],
+                                   short_name=dealer_doc["short_name"],
+                                   st=dealer_doc["st"],
+                                   zip=dealer_doc["zip"])
+            results.append(dealer_obj)
+    return results
 
 # Create a get_dealers_from_cf method to get dealers from a cloud function
 # def get_dealers_from_cf(url, **kwargs):
